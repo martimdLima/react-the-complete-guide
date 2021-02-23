77646459b25bf94e8d6804a479cbda4b8696cbae
Martim Lima <martim.d.lima@protonmail.com>
2021-02-23 14:29:45 +0000

    Understanding the Base Features & Syntax
    Understanding JSX

--- understanding-the-base-features-and-syntax/react-complete-guide/src/App.js
+++ understanding-the-base-features-and-syntax/react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,6 +9,21 @@ class App extends Component {
       </div>
     );
   }
+
+  // this method is responsible for compiling the psuedo-html in the render method
+  // it takes at least 3 arguments, but can take an infinite number of arguments.
+  // the first element choosen to be rendered
+  // the second element is responsible for the configuraiton of the element
+  // the third element is the amount of children, the elements nested in this element
+  //return .createElement('div', null, 'h1', 'Hi, Test React App!!!');
+
+  // render() {
+  //   return React.createElement(
+  //     "div",
+  //     null,
+  //     React.createElement("h1", {className: 'App'}, "Hi, This is a test React App!!!")
+  //   );
+  // }
 }
 
 export default App;
