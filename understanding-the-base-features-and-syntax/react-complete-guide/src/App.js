063bb13193a0fab06ad3f9fc0e6a470c75f2d7d5
Martim Lima <martim.d.lima@protonmail.com>
2021-02-23 18:27:19 +0000

    Understanding the Base Features & Syntax
    Working with Inline Styles

--- understanding-the-base-features-and-syntax/react-complete-guide/src/App.js
+++ understanding-the-base-features-and-syntax/react-complete-guide/src/App.js
@@ -46,10 +46,21 @@ class App extends Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
     return (
       <div className="App">
         <h1>A React App</h1>
-        <button onClick={this.switchNameHandler.bind(this, "Lisbon")}>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, "Lisbon")}
+        >
           Switch Name
         </button>
         {/* <Person name="John" age="24"/> */}
