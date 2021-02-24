29d0bd378db61cfe777c9b96a4d7f76774c48ec0
Martim Lima <martim.d.lima@protonmail.com>
2021-02-24 10:37:06 +0000

    Working with Lists and Conditionals
    Lists & State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,18 +25,13 @@ class App extends Component {
     showePersons: false,
   };
 
-  // it's a good pratice to name all event handlers with the word "handler" to expliclitly mention its purpose
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 23 },
-        { name: "Madrid", age: 34 },
-        { name: "Tokyo", age: 19 },
-      ],
-      otherState: "some value",
-    });
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
+  // it's a good pratice to name all event handlers with the word "handler" to expliclitly mention its purpose
   // rendering content conditionally
   // this method will show/hide names ba
   tooglePersonHandler = () => {
@@ -76,25 +71,15 @@ class App extends Component {
       persons = (
         <div>
           {/* Outputting Lists */}
-          {this.state.persons.map((person) => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
-          {/* <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler("London")}
-            changed={this.nameChangeHandler}
-          >
-            My Hobbies are: Cinema
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          /> */}
         </div>
       );
     }
@@ -112,73 +97,6 @@ class App extends Component {
       </div>
     );
   }
-
-  // this method is responsible for compiling the psuedo-html in the render method
-  // it takes at least 3 arguments, but can take an infinite number of arguments.
-  // the first element choosen to be rendered
-  // the second element is responsible for the configuraiton of the element
-  // the third element is the amount of children, the elements nested in this element
-  //return .createElement('div', null, 'h1', 'Hi, Test React App!!!');
-
-  // render() {
-  //   return React.createElement(
-  //     "div",
-  //     null,
-  //     React.createElement("h1", {className: 'App'}, "Hi, This is a test React App!!!")
-  //   );
-  // }
 }
 
 export default App;
-
-// Using the useState() Hook for State Manipulation
-// const app = (props) => {
-
-//   // useState is the most important React hook. It allows us to manage state in a functiona component.
-
-//   // useState is used by calling it like a function and passing the inital state.
-//   const [ personsState, setPersonsState] = useState({
-//     persons: [
-//       { name: "John", age: 24 },
-//       { name: "Doe", age: 35 },
-//       { name: "Poe", age: 17 },
-//     ]
-//   });
-
-//   const [otherState, setOtherState] = useState({otherState: "some other value"});
-
-//   console.log(personsState, otherState);
-
-//     const switchNameHandler = () => {
-//     setPersonsState({
-//       persons: [
-//         { name: "Texas", age: 23 },
-//         { name: "Madrid", age: 34 },
-//         { name: "Tokyo", age: 19 },
-//       ],
-//     });
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>A React App</h1>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       {/* <Person name="John" age="24"/> */}
-//       {/* <Person name="Doe" age="53">My Hobbies are: Cinema</Person>
-//         <Person name="Poe" age="16"/> */}
-//       <Person
-//         name={personsState.persons[0].name}
-//         age={personsState.persons[0].age}
-//       />
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
-//         My Hobbies are: Cinema
-//       </Person>
-//       <Person
-//         name={personsState.persons[2].name}
-//         age={personsState.persons[2].age}
-//       />
-//     </div>
-//   );
-// };
-
-// export default app;
