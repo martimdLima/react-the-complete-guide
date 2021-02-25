5a991a7a1458d241d1dd70b10b2a3b6005e4911a
Martim Lima <martim.d.lima@protonmail.com>
2021-02-25 18:46:20 +0000

    Diving Deeper into Components & React Internals
    PureComponents instead of shouldComponentUpdate

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 
 import Person from "./Person/Person";
 
@@ -21,7 +21,10 @@ import Person from "./Person/Person";
 
 // export default persons;
 
-class Persons extends Component {
+//React.PureComponent is similar to React.Component.
+// The difference between them is that React.Component doesn’t implement shouldComponentUpdate(),
+// but React.PureComponent implements it with a shallow prop and state comparison.
+class Persons extends PureComponent {
     // getDerivedStateFromProps is invoked right before calling the render method, both on the initial mount and on subsequent updates.
     // static getDerivedStateFromProps(props, state) {
     //     console.log("[Persons.js] getDerivedStateFromProps");
@@ -29,13 +32,17 @@ class Persons extends Component {
     // }
 
     // Use shouldComponentUpdate() to let React know if a component’s output is not affected by the current change in state or props.
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("[Persons.js] shouldComponentUpdate");
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("[Persons.js] shouldComponentUpdate");
 
-        if (nextProps.persons !== this.props.persons) {
-            return true;
-        }
-    }
+    //     if (
+    //         nextProps.persons !== this.props.persons ||
+    //         nextProps.changed !== this.props.changed ||
+    //         nextProps.clicked !== this.props.clicked
+    //     ) {
+    //         return true;
+    //     }
+    // }
 
     // getSnapshotBeforeUpdate() is invoked right before the most recently rendered output is committed to e.g. the DOM.
     getSnapshotBeforeUpdate(previousProps, previousState) {
