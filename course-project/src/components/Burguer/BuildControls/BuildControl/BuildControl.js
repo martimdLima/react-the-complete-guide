c1d80a2d1e03a3d8817a27d1fc929a6c86d6cd26
Martim Lima <martim.d.lima@protonmail.com>
2021-02-26 19:20:31 +0000

    A Real App: The Burger Builder (Basic Version)
    Adding the Build Control Component

---
+++ course-project/src/components/Burguer/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "BuildControl.module.css";
+
+const buildControl = (props) => (
+    <div>
+        <div className={classes.label}>{props.label}</div>
+        <button className={classes.less}>Less</button>
+        <button className={classes.more}>More</button>
+    </div>
+);
+
+export default buildControl;
---
+++ course-project/src/components/Burguer/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./buildControls.module.css";
+
+const buildControls = (props) => (
+    <div>
+
+    </div>
+);
+
+export default buildControls;
