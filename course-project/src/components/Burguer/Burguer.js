dc1287a0471317ea414e4c69d7a040b85161ad34
Martim Lima <martim.d.lima@protonmail.com>
2021-02-26 18:43:33 +0000

    A Real App: The Burger Builder (Basic Version)
    Outputting Burger Ingredients Dynamically

--- course-project/src/components/Burguer/Burguer.js
+++ course-project/src/components/Burguer/Burguer.js
@@ -2,18 +2,30 @@ import React from "react";
 
 import Ingredient from "../Burguer/Ingredient/Ingredient";
 
-import classes from "./Burguer.module.css"
+import classes from "./Burguer.module.css";
 
+
+// transforms an object of key value pairs into an array of burger ingredients
+// where the value of that object is important to decide how many ingredients it's needed and the keys
+// important for which type of ingredient is needed.
 const burguer = (props) => {
+    
+    const transformedIngredients = Object.keys(props.ingredients).map(
+        (ingrKey) => {
+            return [...Array(props.ingredients[ingrKey])].map((_, i) => {
+                return <Ingredient key={ingrKey + i} type={ingrKey} />;
+            });
+        }
+    );
 
-    return(
+    return (
         <div className={classes.Burguer}>
-            <Ingredient type="bread-top"/>
-            <Ingredient type="cheese"/>
-            <Ingredient type="meat"/>
-            <Ingredient type="bread-bottom"/>
+            <Ingredient type="bread-top" />
+            <Ingredient type="cheese" />
+            <Ingredient type="meat" />
+            <Ingredient type="bread-bottom" />
         </div>
     );
-}
+};
 
 export default burguer;
--- course-project/src/components/BurguerBuilder/BurguerBuilder.js
+++ course-project/src/components/BurguerBuilder/BurguerBuilder.js
@@ -6,10 +6,26 @@ import Aux from "../../hoc/Aux";
 
 // This component is responsible for the Burger and Build Controls Components that enables the user to build a burguer
 class BurguerBuilder extends Component {
+
+/*     constructor(props) {
+        super(props);
+        this.state = {...}
+    } */
+
+    state = {
+        ingredients : {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        }
+    }
+
+
     render() {
         return (
             <Aux>
-                <Burguer/>
+                <Burguer ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
