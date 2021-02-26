23f56e8f4453414992a7dec81d06e0fdc80275e4
Martim Lima <martim.d.lima@protonmail.com>
2021-02-26 19:58:33 +0000

    A Real App: The Burger Builder (Basic Version)
    Connecting State to Build Controls

--- course-project/src/components/BurguerBuilder/BurguerBuilder.js
+++ course-project/src/components/BurguerBuilder/BurguerBuilder.js
@@ -5,12 +5,15 @@ import Burguer from "../Burguer/Burguer";
 
 import Aux from "../../hoc/Aux";
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 2,
+    cheese: 1,
+    meat: 3,
+}
+
 // This component is responsible for the Burger and Build Controls Components that enables the user to build a burguer
 class BurguerBuilder extends Component {
-    /*     constructor(props) {
-        super(props);
-        this.state = {...}
-    } */
 
     state = {
         ingredients: {
@@ -19,13 +22,34 @@ class BurguerBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
+        totalPrice: 4
     };
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+    
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+
+        updatedIngredients[type] = updatedCount;
+
+        //const ingredientPrice = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + INGREDIENT_PRICES[type];
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
 
     render() {
         return (
             <Aux>
                 <Burguer ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         );
     }
