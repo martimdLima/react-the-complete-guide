ad66278a9b627a91387dd140bb9e667dc1adf5aa
Martim Lima <martim.d.lima@protonmail.com>
2021-02-26 21:07:29 +0000

    A Real App: The Burger Builder (Basic Version)
    Displaying and Updating the Burger Price

--- course-project/src/components/BurguerBuilder/BurguerBuilder.js
+++ course-project/src/components/BurguerBuilder/BurguerBuilder.js
@@ -10,11 +10,10 @@ const INGREDIENT_PRICES = {
     bacon: 2,
     cheese: 1,
     meat: 3,
-}
+};
 
 // This component is responsible for the Burger and Build Controls Components that enables the user to build a burguer
 class BurguerBuilder extends Component {
-
     state = {
         ingredients: {
             salad: 0,
@@ -22,62 +21,71 @@ class BurguerBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
     };
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
-    
+
         const updatedIngredients = {
-            ...this.state.ingredients
-        }
+            ...this.state.ingredients,
+        };
 
         updatedIngredients[type] = updatedCount;
 
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + INGREDIENT_PRICES[type];
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-    }
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients,
+        });
+    };
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
 
         // to prevent the app from crashing from subtracting negative values, we check if the old count is less or equal to zero
-        if(oldCount <= 0) {
+        if (oldCount <= 0) {
             return;
         }
 
         const updatedCount = oldCount - 1;
-    
+
         const updatedIngredients = {
-            ...this.state.ingredients
-        }
+            ...this.state.ingredients,
+        };
 
         updatedIngredients[type] = updatedCount;
 
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - INGREDIENT_PRICES[type];
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
-    }
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients,
+        });
+    };
 
     render() {
-
-        // disables the less button, by spreading the ingredients object and loop through it, 
+        // disables the less button, by spreading the ingredients object and loop through it,
         // checking if each ingredient is less than zero and returning true or false accordingly
         const disabledInfo = {
-            ...this.state.ingredients
+            ...this.state.ingredients,
         };
 
-        for(let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
         }
 
         return (
             <Aux>
                 <Burguer ingredients={this.state.ingredients} />
-                <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} disabled={disabledInfo}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                />
             </Aux>
         );
     }
