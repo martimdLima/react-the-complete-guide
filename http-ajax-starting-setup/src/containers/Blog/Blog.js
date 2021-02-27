7158ee44dfc3de9cb043257cf27d6fef47bed58e
Martim Lima <martim.d.lima@protonmail.com>
2021-02-27 17:05:29 +0000

    Reaching out to the Web (Http / Ajax)
    Creating a Http Request to GET Data

--- http-ajax-starting-setup/src/containers/Blog/Blog.js
+++ http-ajax-starting-setup/src/containers/Blog/Blog.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import axios from "axios";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
+    componentDidMount() {
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                console.log(response);
+            });
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
