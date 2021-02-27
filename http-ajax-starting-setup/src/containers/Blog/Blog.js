5f81e56d0190908846adad48225128dc3df7af93
Martim Lima <martim.d.lima@protonmail.com>
2021-02-27 19:16:11 +0000

    Reaching out to the Web (Http / Ajax)
    Handling Errors Locally

--- http-ajax-starting-setup/src/containers/Blog/Blog.js
+++ http-ajax-starting-setup/src/containers/Blog/Blog.js
@@ -8,49 +8,59 @@ import "./Blog.css";
 import post from "../../components/Post/Post";
 
 class Blog extends Component {
-
     state = {
         posts: [],
         selectedPostId: null,
-    }
+        error: false,
+    };
 
     componentDidMount() {
         axios
             .get("https://jsonplaceholder.typicode.com/posts")
             .then((response) => {
-
                 const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
+                const updatedPosts = posts.map((post) => {
                     return {
-                    ...post,
-                    author: "Author"
-                    }
+                        ...post,
+                        author: "Author",
+                    };
                 });
 
-                this.setState({posts: updatedPosts})
+                this.setState({ posts: updatedPosts });
                 console.log(response);
+            })
+            .catch((error) => {
+                this.setState({ error: true });
             });
     }
 
     postSeletecHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
+        this.setState({ selectedPostId: id });
+    };
 
     render() {
-
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSeletecHandler(post.id)}/>
-        }
+        let posts = (
+            <p style={{ textAlign: "center" }}>Something went wrong!</p>
         );
 
+        if (!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSeletecHandler(post.id)}
+                    />
+                );
+            });
+        }
 
         return (
             <div>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <section className="Posts">{posts}</section>
                 <section>
-                    <FullPost id={this.state.selectedPostId}/>
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
