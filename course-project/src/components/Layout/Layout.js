ab1fdd0c224ddc45d45e35ccc758544c6d3f310b
Martim Lima <martim.d.lima@protonmail.com>
2021-02-27 15:24:32 +0000

    A Real App: The Burger Builder (Basic Version)
    Reusing the Backdrop
    Adding a Sidedrawer Toggle Button
    Adding a Hamburger Icon

--- course-project/src/components/Layout/Layout.js
+++ course-project/src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
@@ -6,6 +6,7 @@ import Aux from "../../hoc/Aux";
 
 import classes from "./Layout.module.css";
 
+/*
 // The Layout component is responsible for the layout of the app
 const Layout = (props) => (
     <Aux>
@@ -15,4 +16,34 @@ const Layout = (props) => (
     </Aux>
 );
 
+export default Layout; */
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false,
+    };
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    };
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggle={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>{this.props.children}</main>
+            </Aux>
+        );
+    }
+}
 export default Layout;
--- course-project/src/components/Navigation/Toolbar/Toolbar.js
+++ course-project/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Logo from "../../Navigation/Logo/Logo";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 import classes from "./Toolbar.module.css";
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggle} />
         <div className={classes.Logo}>
             <Logo />
         </div>
