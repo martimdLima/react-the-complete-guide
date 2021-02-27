4a911d1753e99c7ec32b05980e0feabcadd95efe
Martim Lima <martim.d.lima@protonmail.com>
2021-02-27 19:23:39 +0000

    Reaching out to the Web (Http / Ajax)
    Adding Interceptors to Execute Code Globally

--- http-ajax-starting-setup/src/index.js
+++ http-ajax-starting-setup/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+
+axios.interceptors.request.use((request) => {
+    console.log(request);
+    return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use((response) => {
+  console.log(response);
+  return response;
+}, error => {
+console.log(error);
+return Promise.reject(error);
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
