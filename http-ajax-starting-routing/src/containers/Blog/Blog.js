34ab289fbfb5361e9951ac9275d6d508897b0868
Martim Lima <martim.d.lima@protonmail.com>
2021-02-28 14:41:00 +0000

    Multi-Page-Feeling in a Single-Page-App: Routing
    Using Links to Switch Pages

--- http-ajax-starting-routing/src/containers/Blog/Blog.js
+++ http-ajax-starting-routing/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
@@ -19,16 +19,23 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li>
-                                <a href="/">Home</a>
+                                <Link to="/">Home</Link>
                             </li>
                             <li>
-                                <a href="/new-post">New Post</a>
+                                <Link
+                                    to={{
+                                        pathname: "/new-post",
+                                        hash: "#submit",
+                                        search: "?quick-submit=true",
+                                    }}>
+                                    New Post
+                                </Link>
                             </li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post"  component={NewPost} />
+                <Route path="/new-post" component={NewPost} />
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 {/* <Posts /> */}
                 {/* <section>
