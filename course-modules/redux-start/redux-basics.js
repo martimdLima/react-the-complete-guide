6bec0ce73a45a52ee5a4895fafde61fe0e0cc817
Martim Lima <martim.d.lima@protonmail.com>
2021-03-04 19:55:55 +0000

    Redux
    Adding Subscriptions

--- course-modules/redux-start/redux-basics.js
+++ course-modules/redux-start/redux-basics.js
@@ -32,9 +32,12 @@ const rootReducer = (currentState = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription - gets triggered whenever the state is updated.
+store.subscribe(() => {
+    console.log("[Subscription]", store.getState());
+});
+
 // Dispatching Action
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
-
-// Subscription
