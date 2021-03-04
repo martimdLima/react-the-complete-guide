902ebee9a129b2b530becf94499230533e14cbc7
Martim Lima <martim.d.lima@protonmail.com>
2021-03-04 19:51:41 +0000

    Redux
    Dispatching Actions

--- course-modules/redux-start/redux-basics.js
+++ course-modules/redux-start/redux-basics.js
@@ -2,23 +2,39 @@
 const redux = require("redux");
 const createStore = redux.createStore;
 
-
 // Initialize the State
 const initialState = {
-    counter: 0
-}
+    counter: 0,
+};
 
 // Create the Reducer
 const rootReducer = (currentState = initialState, action) => {
-    return currentState 
-}
+    //let updatedState = null;
+
+    if (action.type === "INC_COUNTER") {
+        return {
+            ...currentState,
+            counter: currentState.counter + 1,
+        };
+    }
+
+    if (action.type === "ADD_COUNTER") {
+        return {
+            ...currentState,
+            counter: currentState.counter + action.value,
+        };
+    }
+
+    return currentState;
+};
 
 // Create the Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-
 // Dispatching Action
-
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 
 // Subscription
