af5e480b28e7bc49761240fdc3a2c3d45a3716ac
Martim Lima <martim.d.lima@protonmail.com>
2021-03-04 20:19:43 +0000

    Redux
    Connecting the Store to React

--- course-modules/redux-start/src/containers/Counter/Counter.js
+++ course-modules/redux-start/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -22,13 +23,15 @@ class Counter extends Component {
             case 'sub':
                 this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
                 break;
+            default:
+                this.setState( ( prevState ) => { return { counter: prevState.counter } } )
         }
     }
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +41,17 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// mapStateToProps stores a function which expects the state stored in redux as the input 
+// and returns a javascript object which is a map of prop names and slices of the state stored in redux. 
+// Now this function will eventually be executed by the react-redux package.
+const mapStateToProps = (state) => {
+    
+    return {
+        ctr: state.counter
+    };
+}
+
+//  Connect then gives us a container with access to this ctr, our property. This now allows us to output the ctr property.
+
+// Connect is a function that returns a higher order component used on the export,
+export default connect(mapStateToProps)(Counter);
--- course-modules/redux-start/src/index.js
+++ course-modules/redux-start/src/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+
 import { createStore } from "redux";
+import reducer from "./store/reducer";
 
-const store = createStore();
+import "./index.css";
+import App from "./App";
 
+// create the store and pass the reducer to it
+const store = createStore(reducer);
+
+// Provider is a helper component which allows to kind of inject the store
+// into the react components. For hooking up the provider component with the store here,
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
