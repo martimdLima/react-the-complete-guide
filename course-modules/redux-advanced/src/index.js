27f02cf3d954b455e7d6d06d61a6dfcb1e8b9320
Martim Lima <martim.d.lima@protonmail.com>
2021-03-05 22:10:32 +0000

    Redux Advanced
    Adding Middleware

---
+++ course-modules/redux-advanced/src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer,
+});
+
+// create a logger middleware
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+            console.log("[Middleware] Dispatching", action);
+            const result = next(action);
+            console.log("[Middleware] next State", store.getState());
+            return result;
+        };
+    };
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
