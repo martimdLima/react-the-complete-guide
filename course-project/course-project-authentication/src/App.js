904c6f31b4a68ff9931b789caa76cf7fea676815
Martim Lima <martim.d.lima@protonmail.com>
2021-03-07 21:52:07 +0000

    Adding Authentication to our Burger Project
    Guarding Routes

--- course-project/course-project-authentication/src/App.js
+++ course-project/course-project-authentication/src/App.js
@@ -1,41 +1,58 @@
 import React, { Component } from "react";
-import {connect} from "react-redux"
-import { Route, Switch, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurguerBuilder from "./containers/BurguerBuilder/BurguerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
-import * as actions from "./store/actions/index"
+import * as actions from "./store/actions/index";
 
 class App extends Component {
-
     componentDidMount() {
         this.props.onTryAutoSignup();
     }
 
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path='/' component={BurguerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/" component={BurguerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+
         return (
             <div>
-                <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/" component={BurguerBuilder} />
-                    </Switch>
-                </Layout>
+                <Layout>{routes}</Layout>
             </div>
         );
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+    };
+};
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        onTryAutoSignup: () => dispatch(actions.authCheckSate())
-    }
-}
+        onTryAutoSignup: () => dispatch(actions.authCheckSate()),
+    };
+};
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
