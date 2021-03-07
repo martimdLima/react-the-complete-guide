00121e664986381c6a28fe4e79930576150b5b10
Martim Lima <martim.d.lima@protonmail.com>
2021-03-07 15:03:29 +0000

    Adding Authentication to our Burger Project
    Getting a Token from the Backend

--- course-project/course-project-authentication/src/store/actions/auth.js
+++ course-project/course-project-authentication/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
     return {
@@ -18,9 +19,31 @@ export const authFail = () => {
     };
 };
 
+// sign up - "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY
+// sign in - "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + API_KEY
+
 export const auth = (email, password) => {
-    return dispatch => {
+    return (dispatch) => {
         dispatch(authStart());
-        
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true,
+        };
+
+        axios
+            .post(
+                "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDtsfmUnmfvIgXBkiNwj2mHOXRvxzr2jPU",
+                authData
+            )
+            .then((response) => {
+                console.log(response);
+                dispatch(authSuccess());
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch(authFail());
+            });
     };
-}
+};
