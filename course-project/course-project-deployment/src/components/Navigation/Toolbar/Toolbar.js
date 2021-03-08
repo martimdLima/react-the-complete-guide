244f2bf226e3d591b0ece19407fb0f6ac5a394d1
Martim Lima <martim.d.lima@protonmail.com>
2021-03-08 17:28:21 +0000

    Deploying the App to the Web
    Deploying the App to the Web

---
+++ course-project/course-project-deployment/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Logo from "../../Navigation/Logo/Logo";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+
+import classes from "./Toolbar.module.css";
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggle} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems isAuthenticated={props.isAuth}/>
+        </nav>
+    </header>
+);
+
+export default toolbar;
---
+++ course-project/course-project-deployment/src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import burguerBuilderReducer from "./store/reducers/burguerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
+
+import "./index.css";
+
+const composeEnhancers =
+    process.env.NODE_ENV === "development"
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        : null || compose;
+
+const rootReducer = combineReducers({
+    burguerBuilder: burguerBuilderReducer,
+    order: orderReducer,
+    auth: authReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
