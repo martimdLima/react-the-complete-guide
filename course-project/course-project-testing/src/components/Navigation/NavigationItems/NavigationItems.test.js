6e6f32472625ac490ab4088d93ad9ed85093904f
Martim Lima <martim.d.lima@protonmail.com>
2021-03-08 14:59:04 +0000

    Testing
    Jest and Enzyme Documentations

--- course-project/course-project-testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ course-project/course-project-testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -15,11 +15,17 @@ describe("<NavigationItems />", () => {
         wrapper = shallow(<NavigationItems />);
     });
 
-    it("should render two <NavigationItems /> elements if not authenticated", () => {
+    it("should render two <NavigationItem /> elements if not authenticated", () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
-    it("should render three <NavigationItems /> elements if authenticated", () => {
+    it("should render three <NavigationItem /> elements if authenticated", () => {
         wrapper.setProps({isAuthenticated: true})
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+
+    });
+
+    it("should render three <NavigationItems /> elements if authenticated", () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
     });
 });
