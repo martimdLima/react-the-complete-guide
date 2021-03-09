7677fdb9e1bc52471e41d52cb4c4cffe52f7c9ae
Martim Lima <martim.d.lima@protonmail.com>
2021-03-09 12:58:36 +0000

    Next.js
    Styling our App in Next.js

--- course-modules/nextjs/.next/static/chunks/webpack.js
+++ course-modules/nextjs/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8e5a7fc3384ca0e1918f";
+/******/ 	var hotCurrentHash = "f490a11ec3bedd94f4aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
