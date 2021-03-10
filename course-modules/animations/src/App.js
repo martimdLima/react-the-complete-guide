fed34b40c5b016503bcece77b3416cb7ce54fdc6
Martim Lima <martim.d.lima@protonmail.com>
2021-03-10 10:45:23 +0000

    Animations in React Apps
    Using ReactTransitionGroup

--- course-modules/animations/src/App.js
+++ course-modules/animations/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Transition from "react-transition-group/Transition";
+
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -8,6 +10,7 @@ import List from "./components/List/List";
 class App extends Component {
     state = {
         modalIsOpen: false,
+        showBlock: false,
     };
 
     showModal = () => {
@@ -22,6 +25,35 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>React Animations</h1>
+                <button
+                    className="Button"
+                    onClick={() =>
+                        this.setState((prevState) => ({
+                            showBlock: !prevState.showBlock,
+                        }))
+                    }>
+                    Toogle
+                </button>
+                <br />
+
+                <Transition in={this.state.showBlock} mountOnEnter unmountOnExit timeout={1000}>
+                    {(state) => (
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 100,
+                                height: 100,
+                                margin: "auto",
+                                transition: "opacity 1s ease-out",
+                                opacity: state === "exiting" ? 0 : 1,
+                            }}></div>
+                    )}
+                </Transition>
+
+                {/*     {this.state.showBlock ? (
+  
+                ) : null} */}
+
                 {this.state.modalIsOpen ? (
                     <Modal
                         show={this.state.modalIsOpen}
