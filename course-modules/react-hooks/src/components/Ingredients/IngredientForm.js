625152665b8872e35623acb4439a107d4f8863cb
Martim Lima <martim.d.lima@protonmail.com>
2021-03-11 13:27:41 +0000

    React Hooks
    More on useState() & State Updating
    Array Destructuring
    Multiple States

--- course-modules/react-hooks/src/components/Ingredients/IngredientForm.js
+++ course-modules/react-hooks/src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,12 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-    const inputState = useState({
-        title: "",
-        amount: "",
-    });
+    // What is a Hook? A Hook is a special function that lets you “hook into” React features.
+    // When would I use a Hook? If you write a function component and realize you need to add some state to it,
+    // previously you had to convert it to a class. Now you can use a Hook inside the existing function component.
+    // useState is a Hook that lets you add React state to function components.
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
 
     const submitHandler = (event) => {
         event.preventDefault();
@@ -21,23 +23,23 @@ const IngredientForm = React.memo((props) => {
                     <div className="form-control">
                         <label htmlFor="title">Name</label>
                         <input
-                            value={inputState[0].title}
+                            value={enteredTitle}
                             type="text"
                             id="title"
-                            onChange={(event) =>
-                                inputState[1]({ title: event.target.value })
-                            }
+                            onChange={(event) => {
+                                setEnteredTitle(event.target.value);
+                            }}
                         />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">Amount</label>
                         <input
-                            value={inputState[0].amount}
+                            value={enteredAmount}
                             type="number"
                             id="amount"
-                            onChange={(event) =>
-                                inputState[1]({ amount: event.target.value })
-                            }
+                            onChange={(event) => {
+                                setEnteredAmount(event.target.value);
+                            }}
                         />
                     </div>
                     <div className="ingredient-form__actions">
