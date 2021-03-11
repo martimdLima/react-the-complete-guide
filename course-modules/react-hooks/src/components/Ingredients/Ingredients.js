05d42c3447f3fdaf5a3f6e618cef2f177a58ee10
Martim Lima <martim.d.lima@protonmail.com>
2021-03-11 14:44:01 +0000

    React Hooks
    Assignment 6 - Hooks Basics

--- course-modules/react-hooks/src/components/Ingredients/Ingredients.js
+++ course-modules/react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,27 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from "./IngredientList"
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+    const [ingredients, setIngredients] = useState([]);
 
-  const [ingredients, setIngredients] = useState([]);
+    const addIngredientHandler = (ingredient) => {
+        setIngredients((prevIngs) => [
+            ...prevIngs,
+            { id: Math.random().toString(), ...ingredient },
+        ]);
+    };
 
-  const addIngredientHandler = (ingredient) => {
-    setIngredients(prevIngs => [...prevIngs, {id: Math.random().toString(), ...ingredient}])
-  }
+    const onRemoveItem = (igId) => {
+        setIngredients((prevIngs) =>
+            prevIngs.filter((ig) => {
+                return ig.id !== igId;
+            })
+        );
+    };
 
-  return (
-    <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler} />
 
-      <section>
-        <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}}/>
-      </section>
-    </div>
-  );
+            <section>
+                <Search />
+                <IngredientList
+                    ingredients={ingredients}
+                    onRemoveItem={onRemoveItem}
+                />
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
