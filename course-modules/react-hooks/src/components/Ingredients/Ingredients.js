1b84ddbbc4f558ff5913fb077b19f198493d1b01
Martim Lima <martim.d.lima@protonmail.com>
2021-03-11 15:12:05 +0000

    React Hooks
    useEffect() & Loading Data

--- course-modules/react-hooks/src/components/Ingredients/Ingredients.js
+++ course-modules/react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,11 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 
 function Ingredients() {
+
     const [ingredients, setIngredients] = useState([]);
+
+    // The Effect Hook lets you perform side effects in function components
+    // Data fetching, setting up a subscription, and manually changing the DOM in React components are all examples of side effects.
+
+    // If you want to run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([]) as a second argument. 
+    // This tells React that your effect doesn’t depend on any values from props or state, so it never needs to re-run. 
+    // If you pass an empty array ([]), the props and state inside the effect will always have their initial values. 
+    // Passing [] as the second argument is closer to the familiar componentDidMount and componentWillUnmount mental model
+    useEffect(() => {
+        fetch(
+            "https://react-hooks-132f3-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json"
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((responseData) => {
+                const loadedIngredients = [];
+
+                for (const key in responseData) {
+                    loadedIngredients.push(responseData[key]);
+                }
+
+                setIngredients(loadedIngredients);
+            });
+    }, []);
 
     const addIngredientHandler = (ingredient) => {
         fetch(
