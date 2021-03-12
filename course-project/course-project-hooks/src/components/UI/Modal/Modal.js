3dc785f87828a4b634f22e238a69cedf937a6c28
Martim Lima <martim.d.lima@protonmail.com>
2021-03-12 15:56:40 +0000

    Using Hooks in the Burger Builder
    Adding a Custom Error Handling Hook

--- course-project/course-project-hooks/src/components/UI/Modal/Modal.js
+++ course-project/course-project-hooks/src/components/UI/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
@@ -29,36 +29,3 @@ export default React.memo(
         prevProps.show === nextProps.show &&
         prevProps.children === nextProps.children
 );
-/* class Modal extends Component {
-    // this could be a funcional component
-    shouldComponentUpdate(nextProps, nextState) {
-        return (
-            nextProps.show !== this.props.show ||
-            nextProps.children !== this.props.children
-        );
-    }
-
-    render() {
-        return (
-            <Aux>
-                <Backdrop
-                    show={this.props.show}
-                    clicked={this.props.modalClosed}
-                />
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show
-                            ? "translateY(0)"
-                            : "translateY(-100vh)",
-                        opacity: this.props.show ? "1" : "0",
-                    }}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    }
-}
-
-export default Modal;
- */
--- course-project/course-project-hooks/src/hoc/withErrorHandler/withErrorHandler.js
+++ course-project/course-project-hooks/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,38 +1,18 @@
 import React, { useState, useEffect } from "react";
 
+import useHttpErrorHandler from "../../hooks/http-error-handler";
+
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
 
 const WithErrorHandler = (WrappedComponent, axios) => {
+    
     const ErrorWrapper = (props) => {
-        const [error, setError] = useState(null);
-
-        const reqInterceptor = axios.interceptors.request.use((req) => {
-            setError(null);
-            return req;
-        });
-
-        const resInterceptor = axios.interceptors.response.use(
-            (res) => res,
-            (err) => {
-                setError(err);
-            }
-        );
-
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.request.eject(resInterceptor);
-            };
-        }, [reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        };
+        const [error, errorHandler] = useHttpErrorHandler(axios);
 
         return (
             <Aux>
-                <Modal show={error} modalClosed={errorConfirmedHandler}>
+                <Modal show={error} modalClosed={errorHandler}>
                     {error ? error.message : null}
                 </Modal>
                 <WrappedComponent {...props} />
